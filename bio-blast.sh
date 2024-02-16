2be5a3c75058b97bf096464302357b1be8106a29
AllieZheng <zhengyunyun.good@163.com>
2024-02-16 01:55:09 +0000

    add blast script

---
+++ bio-blast.sh
@@ -0,0 +1,20 @@
+  660  ls 
+  661  cd planets
+  662  cd ..
+  663  las
+  664  ls
+  665  cd bio-blast
+  666  git remote add origin https://github.com/AllieZheng/bio-blast.git
+  667  git branch -M main
+  668  history | tail -n 20
+  669  history | tail -n 20 > bio-blast.sh
+  670  ls -a
+  671  nano bio-blast.sh
+  672  histroy
+  673  history
+  674  history -n 369-378
+  675  history --help
+  676  git remote add origin https://github.com/AllieZheng/bio-blast.git
+  677  git branch -M main
+  678  history
+  679  history | tail -n 20 > bio-blast.sh
